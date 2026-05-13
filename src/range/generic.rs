391d937f8b3c4954461f5783bdd5af6fb46edb21
Johannes Pein <johannes.pein@xitaso.com>
2026-05-13 09:42:42 +0200

    chore: fix clippy (#25)
    * fix: clippy::manual_strip
    
    * fix: clippy::collapsible_if
    
    * fix: clippy::derive_ord_xor_partial_ord
    
    * style: fmt

--- src/range/generic.rs
+++ src/range/generic.rs
@@ -318,14 +318,15 @@ impl<V: VersionType> GenericVersionRange<V> {
         // as comparator (or no constraint).
         let mut filter_iter = filtered_constraints.iter().map(|c| c.comparator).peekable();
         while let Some(current) = filter_iter.next() {
-            if let Some(next) = filter_iter.peek() {
-                if current == Equal && !matches!(*next, Equal | GreaterThan | GreaterThanOrEqual) {
-                    return Err(VersError::InvalidRange(format!(
-                        "\"{}\" must not be followed by \"{}\" in a normalized range \
+            if let Some(next) = filter_iter.peek()
+                && current == Equal
+                && !matches!(*next, Equal | GreaterThan | GreaterThanOrEqual)
+            {
+                return Err(VersError::InvalidRange(format!(
+                    "\"{}\" must not be followed by \"{}\" in a normalized range \
                         (ignoring \"!=\")",
-                        current, next,
-                    )));
-                }
+                    current, next,
+                )));
             }
         }
 
