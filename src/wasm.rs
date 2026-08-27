2a4cfbe8846298f536a62330f79760356e3796ea
Thomas Ziemek <thomas.ziemek@xitaso.com>
2026-08-27 16:14:58 +0200

    make wasm tests work

--- src/wasm.rs
+++ src/wasm.rs
@@ -71,3 +71,56 @@ pub fn parse_native(scheme: &str, raw: &str) -> Result<Ts<DynamicVersionRange>, 
     let range = DynamicVersionRange::parse_native(scheme, raw)?;
     Ok(range.into_ts()?)
 }
+
+#[cfg(all(test, target_arch = "wasm32"))]
+mod tests {
+    use std::assert_matches;
+
+    use super::*;
+    use wasm_bindgen_test::*;
+
+    #[wasm_bindgen_test]
+    fn parse_valid_vers_string_returns_expected_result() {
+        let s = "vers:semver/>=1.0.0";
+        let parse_result = parse(s);
+        let ts_type = parse_result.expect("Found an error when parsing a valid vers string");
+        let dynamic_range = ts_type.to_rust().expect("Could not extract rust type");
+        assert_eq!(dynamic_range.versioning_scheme(), "semver");
+        assert_eq!(dynamic_range.constraints().len(), 1);
+    }
+
+    #[wasm_bindgen_test]
+    fn parse_invalid_vers_string_returns_error() {
+        let s = "vers:semver/invalid";
+        let parse_result = parse(s);
+        assert!(parse_result.is_err());
+    }
+
+    #[wasm_bindgen_test]
+    fn parse_native_valid_range_returns_expected_result() {
+        let scheme = "semver";
+        let raw = ">=1.0.0";
+        let parse_result = parse_native(scheme, raw);
+        let ts_type = parse_result.expect("Found an error when parsing a valid native range");
+        let dynamic_range = ts_type.to_rust().expect("Could not extract rust type");
+        assert_eq!(dynamic_range.versioning_scheme(), "semver");
+        assert_eq!(dynamic_range.constraints().len(), 1);
+    }
+
+    #[wasm_bindgen_test]
+    fn parse_native_invalid_range_returns_error() {
+        let scheme = "semver";
+        let raw = "invalid";
+        let parse_result = parse_native(scheme, raw);
+        assert_matches!(parse_result, Err(VersError::InvalidConstraint(_)));
+    }
+
+    #[wasm_bindgen_test]
+    fn contains_version_in_range_returns_true() {
+        let range_str = "vers:semver/>=1.0.0";
+        let version_str = "1.2.3";
+        let range = parse(range_str).expect("Failed to parse range");
+        let result = contains(range, version_str.to_string());
+        assert!(result.expect("Failed to check if version is in range"));
+    }
+}
